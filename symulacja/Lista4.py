04f765d81e594db7a588b26225baa4e4a2b58acb
Mabuza713 <uxbeix@gmail.com>
2024-11-02 17:02:28 +0100

    Sym

--- symulacja/Lista4.py
+++ symulacja/Lista4.py
@@ -11,18 +11,23 @@ class Object:
     def __init__(self,how_much,a, b, m, seed):
         self.how_much = how_much
         self.stats = []
+        self.stats_sim_vals = []
         self.a = a
         self.b = b
         self.m = m
         self.seed = seed
 
     def Random_sim(self, x):
-        return (((self.a * x + self.b) % self.m) / self.m) * 2 - 1
+        return ((self.a * x + self.b) % self.m)
 
     def Simulate(self):
-        self.stats.append(self.Random_sim(self.seed))
+        value = self.Random_sim(self.seed)
+        self.stats_sim_vals.append(value)
+        self.stats.append((value / self.m * 2) - 1)
         for _ in range(0, self.how_much):
-            self.stats.append(self.Random_sim(self.stats[-1]))
+            value = self.Random_sim(self.stats_sim_vals[-1])
+            self.stats_sim_vals.append(value)
+            self.stats.append((value / self.m * 2) - 1)
 
     # Statistics calculations
     def CalculateMeanAndSD(self, verbose=False):
@@ -50,7 +55,7 @@ class Object:
         lista.sort()
         print(f"median {np.median(lista)}")
         if (self.how_much % 2 == 0):
-            return ((lista[self.how_much // 2]) + (lista[self.how_much // 2 - 1]))/2
+            return ((lista[self.how_much // 2]) + (lista[self.how_much // 2 + 1]))/2
         else:
             return (lista[self.how_much // 2])
 
@@ -58,6 +63,7 @@ class Object:
         mean, sd = self.CalculateMeanAndSD()
         print(f"variance {np.var(self.stats)}")
         return sd*sd
+
 
     def CalculateSkewness(self):
         mean, sd = self.CalculateMeanAndSD()
@@ -83,7 +89,7 @@ class Object:
 
 
     def CalculateMode(self):
-        if (self.how_much == len(set(self.stats))):
+        if (self.how_much + 1 == len(set(self.stats_sim_vals))):
             return "No mode"
         else:
             frequency = {}
@@ -115,7 +121,20 @@ class Object:
         
         return (1- interpolation) * lista[lower] + interpolation * lista[upper]
         
-        
+
+
+    
+    def qq_plot(self):
+        sorted_list = sorted(self.stats)
+        stats.probplot(sorted_list, dist="norm", plot=plt)
+        plt.title(f"qq-plot for {self.how_much} elements")
+        plt.show()
+    
+    def boxplot(self):
+        sorted_list = sorted(self.stats)
+        plt.boxplot(sorted_list)
+        plt.title(f"boxplot for {self.how_much} elements")
+        plt.show()
 
     def CheckIfRelevant(self, critical_value):
         mean, sd = self.CalculateMeanAndSD(False)
@@ -162,8 +181,8 @@ class Object:
         
     
 
-object_15 = Object(15, 14684, 29655, 29241, 2)
-object_120 = Object(1200, 14684, 29655, 29241, 2)
+object_15 = Object(15, 1103515245, 12345, 2**31, 2)
+object_120 = Object(120, 1103515245, 12345, 2**31, 2)
 
 object_15.Simulate()
 object_120.Simulate()
@@ -192,17 +211,103 @@ print(object_15.CalculateVariance())
 print(object_120.CalculateVariance())
 object_15.histogram_discreet()
 object_120.histogram_discreet()
-średnich_5000 = []
-for _ in range(int(50000/2)):
-    nowy_obj = Object(1000,14684, 29655, 29241, int(time.time()))
-    nowy_obj.Simulate()
-    
-    srednia, sd = nowy_obj.CalculateMeanAndSD()
-    średnich_5000.append(srednia)
 
 
 
-plt.hist(średnich_5000,bins= 1000)
-plt.show()
 object_15.CheckIfRelevant(2.144)
 object_120.CheckIfRelevant(1.96)
+
+
+def ShapiroWilk(amount_of_samples, amount_of_elemetns):
+    list_sorted = []
+    for _ in range(amount_of_samples):
+        time.sleep(1)
+        temp_obj = Object(amount_of_elemetns, 1103515245, 12345, 2**31, abs(int(time.perf_counter()) * 10000000000000000000))
+        temp_obj.Simulate()
+        mean, sd = temp_obj.CalculateMeanAndSD()
+        list_sorted.append(mean)
+    list_sorted = sorted(list_sorted)
+    W, p = stats.shapiro(list_sorted)
+    print(f"wartość testu shapiro: {W} z p-value: {p}")
+    return W, p
+#ShapiroWilk(250,120)
+
+list_sorted_mean = []
+list_sorted_sd = []
+list_sorted_kurtosis = []
+list_sorted_skweness = []
+list_sorted_variance = []
+
+def CreateMeanMean(amount_of_samples, amount_of_elemetns):
+    for _ in range(amount_of_samples):
+        time.sleep(1)
+        temp_obj = Object(amount_of_elemetns, 1103515245, 12345, 2**31, abs(int(time.perf_counter()) * 10000000000000000000))
+        temp_obj.Simulate()
+        
+        mean, sd = temp_obj.CalculateMeanAndSD()
+        kurtosis = temp_obj.CalculateKurtosis()
+        skewness = temp_obj.CalculateSkewness()
+        var = temp_obj.CalculateVariance()
+        
+        
+        
+        list_sorted_kurtosis.append(kurtosis)
+        list_sorted_skweness.append(skewness)
+        list_sorted_variance.append(var)
+        list_sorted_mean.append(mean)
+        list_sorted_sd.append(sd)
+    
+    list_sorted_mean.sort()
+    list_sorted_sd.sort()
+    list_sorted_kurtosis.sort()
+    list_sorted_skweness.sort()
+    list_sorted_variance.sort()
+    
+def CheckIfRelevant(lista, critical_value, stat_from_list_2):
+    mean = np.mean(lista)
+    sd = np.std(lista, ddof=1)
+    value = sqrt(len(lista)) * (mean - stat_from_list_2) / sd
+    
+    if (abs(value) > critical_value):
+        print(f"for {len(lista)} elements we are rejecting null hipo")
+    else:
+        print(f"for {len(lista)} elements we are NOT rejecting null hip")
+
+CreateMeanMean(100, 120)
+print()
+print()
+print()
+print()
+print()
+print()
+print("test for mean")
+CheckIfRelevant(list_sorted_mean,1.96, 0.06866020977708105)
+
+print("test for sd")
+CheckIfRelevant(list_sorted_sd, 1.96, 0.5855700719712053)
+
+print("test for kurt")
+CheckIfRelevant(list_sorted_kurtosis, 1.96, -1.0533222257919426)
+
+print("test for skew")
+CheckIfRelevant(list_sorted_skweness, 1.96, 0.00011500419053986119)
+
+print("test for var")
+CheckIfRelevant(list_sorted_variance, 1.96, 0.3428923091883625)
+
+
+#średnich_5000 = []
+#for _ in range(int(5000/8)):
+#    time.sleep(1)
+#    nowy_obj = Object(15,1103515245, 12345, 2**31, abs(int(time.perf_counter()) * 10000000000000000000))
+#    nowy_obj.Simulate()
+#    
+#    srednia, sd = nowy_obj.CalculateMeanAndSD()
+#    średnich_5000.append(srednia)
+#    print(srednia)
+#
+#
+#plt.hist(średnich_5000,bins= 1000)
+#plt.show()
+
+
